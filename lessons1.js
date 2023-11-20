5d51372ec0a5e2bbbef2e32f43bf1fc66e7aff41
Joana Akimana <joanarwem2@gmail.com>
2023-11-20 21:14:29 +0400

    first commit

---
+++ lessons1.js
@@ -0,0 +1,93 @@
+let myLess =[
+
+    {
+        "id": 1001,
+        "title": "Mathematics",
+        "location":"Hendon",
+        "price": 175,
+        "image": "images/maths.png",
+        "availableInventory": 15
+       
+      }, 
+      {
+        "id": 1002,
+        "title": "Physics",
+        "location":"Dubai",
+        "price": 150,
+        "image": "images/english.png",
+        "availableInventory": 15
+       
+      }, 
+      {
+        "id": 1003,
+        "title": "Literature",
+        "location":"Liverpool",
+        "price": 100,
+        "image": "images/literature.png",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1004,
+        "title": "English",
+        "location":"Bristol",
+        "price": 120,
+        "image": "images/english.png",
+        "availableInventory": 10
+       
+      }, 
+      {
+        "id": 1005,
+        "title": "ICT",
+        "location":"Tokyo",
+        "price": 180,
+        "image": "images/ict.png",
+        "availableInventory": 10
+       
+      }, 
+      {
+        "id": 1006,
+        "title": "Chemistry",
+        "location":"Sharjah",
+        "price": 150,
+        "image": "images/chemistry.png",
+        "availableInventory": 15
+       
+      }, 
+      {
+        "id": 1007,
+        "title": "Entrepreneurship",
+        "location":"London",
+        "price": 90,
+        "image": "images/entrepreneurship.png",
+        "availableInventory": 8
+       
+      }, 
+      {
+        "id": 1008,
+        "title": "History",
+        "location":"Dubai",
+        "price": 130,
+        "image": "images/history.png",
+        "availableInventory": 10
+       
+      }, 
+      {
+        "id": 1009,
+        "title": "Geography",
+        "location":"London",
+        "price": 100,
+        "image": "images/geography.png",
+        "availableInventory": 15
+       
+      }, 
+      {
+        "id": 1010,
+        "title": "Biology",
+        "location":"Hendon",
+        "price": 150,
+        "image": "images/biology.png",
+        "availableInventory": 10
+       
+      }, 
+]
